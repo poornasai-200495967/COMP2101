8eadff1253fc2ce9e6aad73cb8a59094899ed7f3
poornasai-200495967 <200495967@student.georgianc.on.ca>
2022-10-05 13:46:26 -0400

    My lab 1

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-#My bash lab 2
+#My first bash lab 1
 
-
-#gather my data for my report
-FQDN=$(hostname)
-OSNAMEANDVERSION=$(hostnamectl | grep -h "Operating")
-IPADDRESS=$(hostname -i | awk '{print $1}')
-SpaceAvailableInRoot=$(df / -h | grep "/sda" | awk '{print $4}')  
-
-
-#print out the report using the gathered data
-cat<<EOF
-Report for my PC200495967
-----------
-Fully Qualified Domain Name of my Linux Machine : $FQDN
-----------
-MY Operating System Name and Version : $OSNAMEANDVERSION
-----------
-My linux IP Address : $IPADDRESS
-----------
-My linux Root Filesystem Free Space : $SpaceAvailableInRoot
-----------
---end--
-EOF
+# Hostname of my PC 200495967
+echo " HOSTNAME OF MY PC200495967 "
+hostname
+echo "-----------  "
+# HOSTNAMECTL COMMAND
+echo "HOSTNAMECTL COMMAND "
+hostnamectl
+echo " ---------- "
+# OS NAME & VERSION OF MY LINUX
+echo " OS NAME AND VERSION OF MY LINUX "
+hostnamectl | grep -h "Ope"
+echo " ---------- "
+# IP ADDRESS OF MY LINUX PC - 200495967
+echo " IP ADDRESS OF MY LINUX PC-200495967 "
+hostname -I
+echo " --------- "
+# SPACE IN MY LINUX
+echo "SPACE IN MY LINUX"
+df | grep -h "/sda"
+echo "end"
