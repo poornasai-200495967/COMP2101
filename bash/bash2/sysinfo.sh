2d9aef72428ec4792f52a7b8e1993d6b057c8db1
poornasai-200495967 <200495967@student.georgianc.on.ca>
2022-10-05 13:38:43 -0400

    My lab 2

---
+++ bash/bash2/sysinfo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#My bash lab 2
+
+
+#gather my data for my report
+FQDN=$(hostname)
+OSNAMEANDVERSION=$(hostnamectl | grep -h "Operating")
+IPADDRESS=$(hostname -i | awk '{print $1}')
+SpaceAvailableInRoot=$(df / -h | grep "/sda" | awk '{print $4}')  
+
+
+#print out the report using the gathered data
+cat<<EOF
+Report for my PC200495967
+----------
+Fully Qualified Domain Name of my Linux Machine : $FQDN
+----------
+MY Operating System Name and Version : $OSNAMEANDVERSION
+----------
+My linux IP Address : $IPADDRESS
+----------
+My linux Root Filesystem Free Space : $SpaceAvailableInRoot
+----------
+--end--
+EOF
